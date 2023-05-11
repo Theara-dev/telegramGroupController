4efb67ce3c70b8e74da7fdaac443c07dd909eb64
theara-ztoa <theara.ztoa@gmail.com>
2023-05-11 15:34:40 +0700

    update 2

--- src/auth/start.js
+++ src/auth/start.js
@@ -2,13 +2,21 @@ module.exports = (bot) => {
   bot.on("new_chat_members", (ctx) => {
     const title = ctx.chat.title;
     const username = ctx.from.first_name;
+    let lastname = isLastName(ctx.from.last_name);
 
-    ctx.reply(`សួស្ដី ${username} សូមស្វាគមន៍មកកាន់ ${title}`);
+    ctx.reply(`សួស្ដី ${username + " " + lastname}សូមស្វាគមន៍មកកាន់ ${title}`);
+    console.log(ctx.from);
+    console.log(ctx.chat);
   });
 
   bot.on("left_chat_member", (ctx) => {
     const username = ctx.from.first_name;
+    let lastname = isLastName(ctx.from.last_name);
 
-    ctx.reply(`ជំរាបលា ${username} ជួបគ្នាឱកាសក្រោយ.👋`);
+    ctx.reply(`ជំរាបលា ${username + " " + lastname}.👋`);
   });
+
+  function isLastName(lastName) {
+    return lastName ? lastName : "";
+  }
 };
